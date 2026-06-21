1180b089aaad5d9b397b7735795b8fe3a57191e5
LucasSalus <lucas01salustriano@gmail.com>
2026-06-20 21:00:28 -0300

    Update main.py
    Inclusão de calculo e resultado segundo Newton

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from interpolacao import interpolacao_lagrange
+from interpolacao import interpolacao_lagrange, interpolacao_newton
 
 # Dados do Dataset do Drone (Enunciado do Problema 1)
 tempos = [1.0, 2.0, 3.0, 4.0, 5.0]
@@ -8,7 +8,10 @@ altitudes = [10.0, 25.0, 45.0, 70.0, 105.0]
 tempo_falha = 3.5
 
 # Executa o algoritmo que criamos do zero
-altitude_estimada = interpolacao_lagrange(tempos, altitudes, tempo_falha)
+res_lagrange = interpolacao_lagrange(tempos, altitudes, tempo_falha)
+res_newton = interpolacao_newton(tempos, altitudes, tempo_falha)
+
 
 print("--- SISTEMA DE TELEMETRIA DO DRONE ---")
-print(f"Altitude estimada no instante t = {tempo_falha}s: {altitude_estimada:.2f} metros")
+print(f"Altitude estimada por Lagrange = {tempo_falha}s: {res_lagrange:.2f} metros")
+print(f"Altitude por Newton   (t={tempo_falha}s): {res_newton:.4f} metros\n")
