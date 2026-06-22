12f21ef646d3e0a71c194545a623af79e86f62c6
LucasSalus <lucas01salustriano@gmail.com>
2026-06-21 22:20:13 -0300

    Atualizar o integracao.py

--- integracao.py
+++ integracao.py
@@ -1,42 +1,29 @@
-def regra_trapezios(f, a, b, n):
-    h = (b - a) / n
-    soma = 0.5 * (f(a) + f(b))
+def regra_trapezios_pontos(y_pontos, h):
+    
+    n = len(y_pontos) - 1
+    soma = 0.5 * (y_pontos[0] + y_pontos[-1])
     
     for i in range(1, n):
-        soma += f(a + i * h)
+        soma += y_pontos[i]
         
     return soma * h
 
 
-def regra_simpson_13(f, a, b, n):
+def regra_simpson_13_pontos(y_pontos, h):
+ 
+    n = len(y_pontos) - 1
     if n % 2 != 0:
-        raise ValueError("O número de intervalos (n) para a Regra de 1/3 de Simpson deve ser par.")
+        raise ValueError("Para a Regra de 1/3 de Simpson, o número de intervalos deve ser par.")
         
-    h = (b - a) / n
-    soma = f(a) + f(b)
+    soma = y_pontos[0] + y_pontos[-1]
     
     for i in range(1, n):
-        x = a + i * h
         if i % 2 == 0:
-            soma += 2 * f(x)  
+            soma += 2 * y_pontos[i]  
         else:
-            soma += 4 * f(x)  
+            soma += 4 * y_pontos[i]  
             
     return soma * (h / 3)
 
 
-def regra_simpson_38(f, a, b, n):
-    if n % 3 != 0:
-        raise ValueError("O número de intervalos (n) para a Regra de 3/8 de Simpson deve ser múltiplo de 3.")
-        
-    h = (b - a) / n
-    soma = f(a) + f(b)
     
-    for i in range(1, n):
-        x = a + i * h
-        if i % 3 == 0:
-            soma += 2 * f(x)  
-        else:
-            soma += 3 * f(x)  
-            
-    return soma * (3 * h / 8)
